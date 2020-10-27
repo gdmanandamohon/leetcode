80dc766f5bc890eb39c8829539f4b9c837f11515
Ananda Mohon Ghosh <gdm.anandamohon@gmail.com>
2020-10-27 00:23:29 -0400

    day chalalange complete 1510 logics updated

--- 1510.py
+++ 1510.py
@@ -5,17 +5,18 @@ People who are happy makes other happy. I am gonna finish it slowly but definite
 
 class Solution:
     def winnerSquareGame(self, n: int) -> bool:
-        n+=1
-        print(n)
-        self.wl = [False for i in range(n)]
-        self.wl[1] =True
-        for i in range(2, n):
-            root = int(pow(i, 0.5))
-            #print(i,root)
-            for j in range(root, 0, -1):
-                t= i -j*j
-                #print("  => ", j, t)
-                if self.wl[t] == False:
-                    self.wl[i] = True
-                    break;
-        return self.wl[n-1]
+        dp = [False for _ in range(n+1)]
+        dp[1] =True
+        for i in range(int(math.sqrt(n))): dp[i*i] = True
+        for i in range(2, n+1):
+            c = True
+            for j in range(1, int(pow(i, 0.5))+1):
+                c = c & dp[j*j] & dp[i-j*j]
+                if not c:break
+            if not c: dp[i] = True
+            #print(dp[1:])
+        return dp[n]
+            
+        
+                
+        
